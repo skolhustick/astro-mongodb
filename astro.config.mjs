31e186254df41780ef837a96a07c10d97943bb09
eshwarenm <eshwarenm@gmail.com>
2022-09-03 23:09:55 +0800

    readme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
-
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: node(),
-  integrations: [svelte()]
+  integrations: [svelte()],
 });
